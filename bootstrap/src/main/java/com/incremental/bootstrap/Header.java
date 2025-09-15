aa9f6f6773b97f0f1d9a74e49e2c182972bab9f4
zhaoyunxing <zhaoyunxing@apache.org>
2025-09-15 20:25:27 +0800

    init

---
+++ bootstrap/src/main/java/com/incremental/bootstrap/Header.java
@@ -0,0 +1,13 @@
+package com.incremental.bootstrap;
+
+import org.incremental.shared.BasicHeader;
+
+/**
+ * @author docker
+ */
+public class Header {
+
+    public BasicHeader getHeader() {
+        return BasicHeader.builder().build();
+    }
+}
---
+++ shared/src/main/java/org/incremental/shared/BasicHeader.java
@@ -0,0 +1,17 @@
+package org.incremental.shared;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author docker
+ */
+@Getter
+@Setter
+@Builder
+public class BasicHeader {
+
+    private String table;
+    private String bizId;
+}
